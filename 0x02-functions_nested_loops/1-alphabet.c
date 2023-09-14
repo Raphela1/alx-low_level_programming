e56b0a61140f104c05eca14b0dbf32a780bfc386
Raphela1 <sochimaikeme@gmail.com>
2023-09-13 21:19:07 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this is a function
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+char h;
+
+for (h = 'a'; h <= 'z'; h++)
+{
+_putchar(h);
+}
+_putchar('\n');
+}
